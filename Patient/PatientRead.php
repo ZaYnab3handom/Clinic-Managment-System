7ca40563079b57bee56c0fde2e744f5dfb816808
Mariovictore <119739813+Mariovictore@users.noreply.github.com>
2023-02-28 15:13:34 +0200

    Update PatientRead.php
    Patient Read (Done for the data given)

--- Patient/PatientRead.php
+++ Patient/PatientRead.php
@@ -1,28 +1,23 @@
-<?php 
+?php 
   
        include_once("../dbConnection.php");
         //Read From Patient
-        $readSql = "select * from patient"; 
-        $readResult = mysqli_query($connection, $readSql);
-        while($data = mysqli_fetch_array($readResult)) {
+        $readSql = "SELECT * FROM users JOIN patient WHERE users.national_id=patient.user_id"; 
+        $readResult1 = mysqli_query($connection, $readSql);
+        $currentDate = date("d-m-Y");
+
+        while($data = mysqli_fetch_array($readResult1)) {
             echo "<tr>";
-            echo "<td>".$data["national-id"]."</td>";
+            echo "<td>".$data["national_id"]."</td>";
             echo "<td>".$data["name"]."</td>";
             echo "<td>".$data["gender"]."</td>";
             echo "<td>".$data["mobile"]."</td>";
-            echo "<td>".$data["age"]."</td>";
-            echo "<td>".$data["blood type"]."</td>";
-
-            echo "<td> <a href='delete.php?user=$data[id]'> <i class='bi bi-trash text-danger'></i> </a> </td>";
-            echo "<td> <a href='edit.php?user=$data[id]'> <i class='bi bi-pencil-square text-warning'></i> </a> </td>";
-
+            $age = date_diff(date_create($data["birthDate"]), date_create($currentDate));
+            echo "<td>".$age->format("%y")."</td>";
+            echo "<td>".$data["blood_type"]."</td>";
+            echo "<td> <a href='edit'></a> </td>";
             echo "</tr>";
         
         }
-      
-       
-   
-    
-           
 
 ?>
