83f55ec431941248f9d0159f93a37269e9fd913a
mariovictor <marioi.victor1@gmail.com>
2023-03-05 16:27:03 +0200

    .

--- Doctor/DoctorRead.php
+++ Doctor/DoctorRead.php
@@ -4,16 +4,16 @@
         $readSql = "SELECT * FROM users WHERE type='Doctor'"; 
         $readResult1 = mysqli_query($connection, $readSql);
 
-        while($Rdata = mysqli_fetch_array($readResult1)) {
+        while($data = mysqli_fetch_array($readResult1)) {
             echo "<tr>";
-            echo "<td>".$Rdata["name"]."</td>";
-            echo "<td>".$Rdata["name"]."</td>";
+            echo "<td>".$data["name"]."</td>";
+            echo "<td>".$data["name"]."</td>";
             /*echo "<td>".$data["schedule.dayDate"]."</td>";
             echo "<td>".$data["schedule.start"]."</td>";
             echo "<td>".$data["schedule.end"]."</td>";
             echo "<td>".$data["schedule.room_no"]."</td>";*/
-            echo "<td> <a href='editdoctor.html?national_id=$Rdata[national_id]'>edit</a> </td>";
-            echo "<td> <a href='../appointment/appointmentForm.html?user=$Rdata[national_id]'>Add Schedule</a></td>";
+            echo "<td> <a href='editdoctor.html?national_id=$data[national_id]'>edit</a> </td>";
+            echo "<td> <a href='../appointment/appointmentForm.html?user=$data[national_id]'>Add Schedule</a></td>";
             echo "</tr>";
         
         }
